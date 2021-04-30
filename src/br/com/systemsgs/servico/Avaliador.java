f2a7a6b989e656f3869042dc92a179897b753c58
GuilhermeJWT <guiromanno@gmail.com>
2021-04-30 15:35:31 -0300

    Terminando a primeira parte

--- src/br/com/systemsgs/servico/Avaliador.java
+++ src/br/com/systemsgs/servico/Avaliador.java
@@ -1,5 +1,10 @@
 package br.com.systemsgs.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.systemsgs.Lance;
 import br.com.systemsgs.Leilao;
 
@@ -7,6 +12,7 @@ public class Avaliador {
 	
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
+	private List<Lance> maiores;
 	
 	public void avalia(Leilao leilao) {
 		
@@ -17,6 +23,26 @@ public class Avaliador {
 			
 		}
 		
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+
+			@Override
+			public int compare(Lance o1, Lance o2) {
+				
+			if (o1.getValor() < o2.getValor()) return 1;
+			if (o1.getValor() > o2.getValor()) return -1;
+			return 0;	
+			
+			}
+			
+		});
+		
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+		
+	}
+	
+	public List<Lance> getTresMaiores() {
+		return maiores;
 	}
 	
 	public double getMaiorLance() {
--- src/br/com/systemsgs/test/TestDoAvaliador.java
+++ src/br/com/systemsgs/test/TestDoAvaliador.java
@@ -1,5 +1,9 @@
 package br.com.systemsgs.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,6 +16,7 @@ public class TestDoAvaliador {
 	
 	@Test
 	public void deveEntenderLanceEmOrdem() {
+		
 		Usuario gui = new Usuario("Guilherme");
 		Usuario lavinia = new Usuario("Lavinia");
 		Usuario marcela = new Usuario("Marcela");
@@ -27,8 +32,50 @@ public class TestDoAvaliador {
 		double maiorEsperado = 500;
 		double menorEsperado = 250;
 		
-		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
-		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		
+	}
+	
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+		
+		Usuario gui = new Usuario("Guilherme");
+		Leilao leilao = new Leilao("Macbook");
+		
+		leilao.propoe(new Lance(gui, 1000.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
+		
+	}
+	
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		
+		Usuario gui = new Usuario("Guilherme");
+		Usuario lavinia = new Usuario("Lavinia");
+		
+		Leilao leilao = new Leilao("Iphone");
+		
+		leilao.propoe(new Lance(gui, 100.0));
+		leilao.propoe(new Lance(lavinia, 200.0));
+		leilao.propoe(new Lance(gui, 300.0));
+		leilao.propoe(new Lance(lavinia, 400.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		leiloeiro.getTresMaiores();
+		
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		assertEquals(3, maiores.size());
+		assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
+		assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
+		assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
 		
 	}
 
