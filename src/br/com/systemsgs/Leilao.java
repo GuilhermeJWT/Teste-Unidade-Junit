1623b35beb563c72b138f935d97dba46a9f0ef0d
GuilhermeJWT <guiromanno@gmail.com>
2021-05-03 09:12:27 -0300

    Subindo mais alguns test

--- src/br/com/systemsgs/Leilao.java
+++ src/br/com/systemsgs/Leilao.java
@@ -15,7 +15,30 @@ public class Leilao {
 	}
 	
 	public void propoe(Lance lance) {
-		lances.add(lance);
+		
+		int total = quantidadeLances(lance.getUsuario());
+		
+		if(lances.isEmpty() || podeDarLance(lance.getUsuario())) {
+			lances.add(lance);
+		}
+		
+	}
+
+	private boolean podeDarLance(Usuario usuario) {
+		return !ultimoLanceDado().getUsuario().equals(usuario) && quantidadeLances(usuario);
+	}
+
+	private int quantidadeLances(Usuario usuario) {
+		int total = 0;
+		
+		for (Lance l : lances) {
+			if(l.getUsuario().equals(usuario)) total++;
+		}
+		return total;
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size()-1);
 	}
 
 	public String getDescricao() {
--- src/br/com/systemsgs/test/LeilaoTest.java
+++ src/br/com/systemsgs/test/LeilaoTest.java
@@ -33,5 +33,48 @@ public class LeilaoTest {
 		assertEquals(3000, leilao.getLances().get(1).getValor(), 0.0001);
 		
 	}
+	
+	@Test
+	public void naoDeveAceitarDoisLancesSeguidosDoMesmoUsuario() {
+		
+		Leilao leilao = new Leilao("Computador Gamer");
+		Usuario gui = new Usuario("Guilherme Santos");
+		
+		leilao.propoe(new Lance(gui, 2000));
+		leilao.propoe(new Lance(gui, 3000));
+		
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
+		
+	}
+	
+	@Test
+	public void naoDeveAceitarMaisDe5LancesDoMesmoUsuario() {
+		
+		Leilao leilao = new Leilao("Macbook");
+		Usuario gui = new Usuario("Guilherme Santos");
+		Usuario lavinia = new Usuario("Lavinia");
+		
+		leilao.propoe(new Lance(gui, 2000.0));
+		leilao.propoe(new Lance(lavinia, 2000.0));
+		
+		leilao.propoe(new Lance(gui, 4000.0));
+		leilao.propoe(new Lance(lavinia, 1000.0));
+		
+		leilao.propoe(new Lance(gui, 3000.0));
+		leilao.propoe(new Lance(lavinia, 8000.0));
+		
+		leilao.propoe(new Lance(gui, 6000.0));
+		leilao.propoe(new Lance(lavinia, 5000.0));
+		
+		leilao.propoe(new Lance(gui, 4000.0));
+		leilao.propoe(new Lance(lavinia, 3000.0));
+		
+		leilao.propoe(new Lance(gui, 10000.0));
+		
+		assertEquals(10, leilao.getLances().size());
+		assertEquals(11000.0, leilao.getLances().get(leilao.getLances().size()-1).getValor(), 0.0001);
+		
+	}
 
 }
